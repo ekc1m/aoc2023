1c653a8f58cb1a2ed7184ed3f5516ffd2e23e119
ekcim <micke.simolin@gmail.com>
2024-03-27 23:06:25 +0200

    added some error handling

--- dayone/dayone1.py
+++ dayone/dayone1.py
@@ -2,30 +2,32 @@
 import sys
 
 
-def sum_of_calibration_values(filename: str):
-    with open(filename, "r") as f:
-        lines = f.read().split("\n")
-        first = None
-        last = None
-        numbers = []
-        for i in lines:
-            for j in i:
-                if j.isdigit():
-                    if first == None:
-                        first = j
-                        last = j
-                    last = j
-            if first != None and last != None:
-                numbers.append(first + last)
+def sum_of_calibration_values(filename: str) -> int:
+    try:
+        with open(filename, "r") as f:
+            lines = f.read().split("\n")
             first = None
             last = None
-        return sum(list(map(int, numbers)))
+            numbers = []
+            for i in lines:
+                for j in i:
+                    if j.isdigit():
+                        if first == None:
+                            first = j
+                            last = j
+                        last = j
+                if first != None and last != None:
+                    numbers.append(first + last)
+                first = None
+                last = None
+            return sum(list(map(int, numbers)))
+    except:
+        sys.exit("File <%s> not found" % filename)
 
 
 def main() -> None:
     if len(sys.argv) != 2:
-        print(f"Usage: %s <filename>" % sys.argv[0])
-        sys.exit(1)
+        sys.exit(f"Usage: %s <filename>" % sys.argv[0])
     filename = sys.argv[1].rstrip()
     print(sum_of_calibration_values(filename))
 
--- dayone/dayone2.py
+++ dayone/dayone2.py
@@ -5,24 +5,26 @@ import sys
 def sum_of_calibration_values(filename: str) -> int:
     numbers = []
     letters = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    with open(filename, "r") as f:
-        lines = f.read().split("\n")
-        for line in lines:
-            digs = []
-            for i, j in enumerate(line):
-                if j.isdigit():
-                    digs.append(j)
-                for x, y in enumerate(letters):
-                    if line[i:].startswith(y):
-                        digs.append(str(x + 1))
-            numbers.append(int(digs[0] + digs[-1]))
+    try:
+        with open(filename, "r") as f:
+            lines = f.read().split("\n")
+            for line in lines:
+                digs = []
+                for i, j in enumerate(line):
+                    if j.isdigit():
+                        digs.append(j)
+                    for x, y in enumerate(letters):
+                        if line[i:].startswith(y):
+                            digs.append(str(x + 1))
+                numbers.append(int(digs[0] + digs[-1]))
+    except:
+        sys.exit(f"File <%s> not found" % filename)
     return sum(numbers)
 
 
 def main() -> None:
     if len(sys.argv) != 2:
-        print(f"Usage: %s <filename>" % sys.argv[0])
-        sys.exit(1)
+        sys.exit(f"Usage: %s <filename>" % sys.argv[0])
     filename = sys.argv[1].rstrip()
     print(sum_of_calibration_values(filename))
 
